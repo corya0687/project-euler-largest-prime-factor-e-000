d88881c6ebd72bc4f8193bd1c4fa605f01c92790
corya0687 <corya0687@gmail.com>
2015-12-07 22:07:47 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,38 @@
-# Enter your procedural solution here!
+=begin
+1. find first prime factor
+2. Set loop to only look up to the num devided by last prime found.
+3. check if last prime is the same as prime just found. if so. put that in array and return
+=end
+
+def is_this_prime(i)
+  check = (2..i-1).all? {|n| !(i % n == 0)}
+  check
+end
+
+def find_poten_primes(num)
+  i = 1
+  potential_primes =  Array.new
+
+  while i < num
+
+   if num  % i == 0
+     if is_this_prime(i)
+        potential_primes.push(i)
+        num = num/i
+       puts "num= #{num}"
+     end
+   end
+
+      i += 2
+    if i == num
+      potential_primes.push(i)
+    end
+  end
+  return  potential_primes
+end
+
+  def largest_prime_factor(num)
+    primes = find_poten_primes(num)
+    primes.max
+  end
+
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,48 @@
+class LargestPrimeFactor
+  def initialize(num)
+    @num = num
+  end
+
+  def num= (num)
+    @num = num
+  end
+
+  def num
+    @num
+  end
+
+
+
+  def is_this_prime(i)
+    check = (2..i-1).all? {|n| !(i % n == 0)}
+    check
+  end
+
+  def find_poten_primes
+    i = 1
+    potential_primes =  Array.new
+
+    while i < @num
+
+     if @num  % i == 0
+       if is_this_prime(i)
+          potential_primes.push(i)
+          @num = @num/i
+
+       end
+     end
+
+        i += 2
+      if i == @num
+        potential_primes.push(i)
+      end
+    end
+    return  potential_primes
+  end
+
+  def number
+    primes = find_poten_primes
+    primes.max
+  end
+end
 # Enter your object-oriented solution here!
